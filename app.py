45447d371998813e503777a004d2df477c3aff00
AntunesRafael <119271166+AntunesRafael@users.noreply.github.com>
2022-11-28 17:14:08 -0300

    tentativa 2 da automação com python
    apesar de conseguir resolver os problemas, o software que eu planejava interagir não aceita o "pyautogui"

--- app.py
+++ app.py
@@ -1,20 +1,26 @@
 from time import sleep
-import pyautogui
+import pyautogui 
 
-sleep(2)
+#ir para janela
+pyautogui.hotkey('alt','tab')
 
-with open('DATAS.txt') as datas:
-    for data in datas:
-        datade = data.split(',')[2]
-        dataate = data.split(',')[2]
-        mes = data.split(',')[2]
-#clicar no de e escrever a data
-pyautogui.click(402, 455, duration=1); pyautogui.write(datade)
-#clicar no até e escrever data e clicar para print
-pyautogui.click(446, 489, duration=1); pyautogui.write(dataate); pyautogui.click(363, 585, duration=1)
-#clicar em ok 
-pyautogui.click(489, 581, duration=1)
-#clicar em nome e digitar mes
-pyautogui.click(689, 461, duration=1); pyautogui.write(mes)
-#clicar em salvar
-pyautogui.click(709, 509, duration=1)
+sleep(1)
+
+#exportar e configurar datas como var
+with open('datas.txt','r') as data:
+    for linha in data:
+        dti = linha.split(',')[0]
+        dtf = linha.split(',')[1]
+        mes = linha.split(',')[2]
+    #clicar e adicionar data 
+    pyautogui.click(358,480,duration=1)
+    pyautogui.write(dti)
+    pyautogui.press('enter')
+    pyautogui.write(dtf)
+    pyautogui.click(361,567)
+    sleep(0.9)
+    pyautogui.click(691,525)
+    sleep(0.4)
+    pyautogui.click(776,475)
+    pyautogui.write(mes)
+    pyautogui.press('enter')
